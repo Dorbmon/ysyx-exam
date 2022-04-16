74b374268c8c793804226b9a3903a12aea31c25c
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-16 19:57:44 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:44 up  1:53,  0 users,  load average: 0.26, 0.71, 0.46

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include <memory>
 
-int main() {
-  printf("Hello, ysyx!\n");
+#include "Vtop.h"
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+double sc_time_stamp() { return 0; }
+
+int main(int argc, char **argv, char **env) {
+  if (false && argc && argv && env) {
+  }
+  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+  contextp->traceEverOn(true);
+  contextp->commandArgs(argc, argv);
+  VerilatedVcdC *vcd = new VerilatedVcdC();
+  const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+  top->trace(vcd, 0);
+  vcd->open("data.vcd");
+  int time = 0;
+  while (time < 20 && !contextp->gotFinish()) {
+    contextp->timeInc(1);
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;top->b = b;
+    top->eval();
+    vcd->dump(time);
+    printf("%d %d %d\n",a,b,top->f);
+    //std::cout << a << " " << b << " " << top->f << std::endl;
+    time ++;
+  }
+  top->final();
+  vcd->close();
   return 0;
 }
