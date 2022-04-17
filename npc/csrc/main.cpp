cf4c44228829b8304e70356f2dddfe7ecd9389d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 09:33:29 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:33:29 up 12 min,  0 users,  load average: 0.53, 0.22, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -7,15 +7,6 @@ double sc_time_stamp() { return 0; }
 const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 Vtop* top = new Vtop{contextp.get(), "TOP"};
 void nvboard_bind_all_pins(Vtop* top);
-void single_cycle() {
-  top->eval();
-  top->eval();
-}
-void reset(int n) {
-  //top->rst = 1;
-  while (n -- > 0) single_cycle();
- // top->rst = 0;
-}
 int main(int argc, char **argv, char **env) {
   nvboard_bind_all_pins(top);
   nvboard_init();
@@ -27,12 +18,11 @@ int main(int argc, char **argv, char **env) {
   top->trace(vcd, 0);
   vcd->open("data.vcd");
   int time = 0;
-  reset(10);
   while (!contextp->gotFinish()) {
-    contextp->timeInc(1);
-    vcd->dump(time);
-    time ++;
-    single_cycle();
+    //contextp->timeInc(1);
+    //vcd->dump(time);
+    //time ++;
+    top->eval();
     nvboard_update();
   }
   top->final();
