612603673765cf42a46c59379a92eeec45f39356
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-17 13:41:14 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:41:13 up  4:20,  0 users,  load average: 0.47, 0.33, 0.20

--- npc/build/auto_bind.cpp
+++ npc/build/auto_bind.cpp
@@ -6,4 +6,5 @@ void nvboard_bind_all_pins(Vtop* top) {
 	nvboard_bind_pin( &top->e, BIND_RATE_SCR, BIND_DIR_IN , 1, SW8);
 	nvboard_bind_pin( &top->y, BIND_RATE_SCR, BIND_DIR_OUT, 3, LD2, LD1, LD0);
 	nvboard_bind_pin( &top->indicator, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD4);
+	nvboard_bind_pin( &top->HEX0, BIND_RATE_SCR, BIND_DIR_OUT, 7, SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G);
 }
