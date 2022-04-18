2410271fb11f42f175c83bc6974e30597822d56d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-18 17:31:57 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:31:57 up 1 day, 36 min,  0 users,  load average: 0.15, 0.20, 0.16

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -3,7 +3,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
-
+#include <utils.h>
 static int is_batch_mode = false;
 
 void init_regex();
@@ -34,7 +34,8 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  return 0;
+  nemu_state.state = NEMU_QUIT;
+  return -1;
 }
 
 static int cmd_help(char *args);
