cbd4ffd393a91763e734883a47456723f388f7a4
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 10:50:04 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:50:04 up 14:18,  0 users,  load average: 0.30, 0.27, 0.14

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -33,9 +33,9 @@ int strcmp(const char *s1, const char *s2) {
   if (l1 != l2) {
     return l1 - l2;
   }
-  for (size_t index = 0;index < l1;++ index) {
-    if (s1 [index] != s2 [index]) return s1 [index] - s2 [index];
-  }
+  // for (size_t index = 0;index < l1;++ index) {
+  //   if (s1 [index] != s2 [index]) return s1 [index] - s2 [index];
+  // }
   return 0;
 }
 
