df1ae4581bea2e1bc08dd6e8ec1885a215ed9883
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 10:28:58 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:28:58 up 13:57,  0 users,  load average: 0.18, 0.14, 0.10

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -8,7 +8,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_B,
   TYPE_N, // none
 };
 
@@ -23,6 +23,8 @@ static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12) << 12, 32) ; }
 static word_t immS(uint32_t i) { return SEXT((BITS(i, 31, 25) << 5) | BITS(i, 11, 7), 12); }
 static word_t immJ(uint32_t i) {return  SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 30, 21) << 1) | (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12), 21); }
+static word_t immB(uint32_t i) {return (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1) | (BITS(i, 7, 7) << 11);}
+
 #define kDisplayWidth 32
 void pBin(long int x)
 {
@@ -50,6 +52,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
     case TYPE_J: src1I(immJ(i)); break;
+    case TYPE_B: destI(immB(i)); src1R(rs1); src2R(rs2); break;
   }
 }
 void rjal(Decode *s, word_t dest,word_t src1,word_t src2) {
@@ -82,7 +85,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, raddi(s, dest, src1, src2));
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, rjal(s, dest, src1, src2));
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, rjalr(s, dest, src1, src2));
-  
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if (src1 == src2) s->dnpc += dest;);
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
