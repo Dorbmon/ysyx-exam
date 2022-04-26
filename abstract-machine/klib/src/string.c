be8f64c3c494dd8efd2f636ea60df8bbada3490b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 23:53:55 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:53:55 up 32 min,  0 users,  load average: 0.23, 0.71, 0.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,18 +1,20 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  for (size_t i = 0;;i ++) {
-    if (s [i] == '\0') return i;
+  for (size_t i = 0;; i++) {
+    if (s[i] == '\0')
+      return i;
   }
 }
 
 char *strcpy(char *dst, const char *src) {
-  char* res = dst;
-  while ((*(dst++) = *(src++)) != '\0');
+  char *res = dst;
+  while ((*(dst++) = *(src++)) != '\0')
+    ;
   return res;
 }
 
@@ -22,19 +24,22 @@ char *strncpy(char *dst, const char *src, size_t n) {
 
 char *strcat(char *dst, const char *src) {
   size_t i = 0;
-  while (dst [i] != '\0') ++ i;
-  while (*src != '\0') dst [i++] = *(src++);
-  dst [i] = '\0';
+  while (dst[i] != '\0')
+    ++i;
+  while (*src != '\0')
+    dst[i++] = *(src++);
+  dst[i] = '\0';
   return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  size_t l1 = strlen(s1),l2 = strlen(s2);
+  size_t l1 = strlen(s1), l2 = strlen(s2);
   if (l1 != l2) {
     return l1 - l2;
   }
-  for (size_t index = 0;index < l1;++ index) {
-    if (s1 [index] != s2 [index]) return s1 [index] - s2 [index];
+  for (size_t index = 0; index < l1; ++index) {
+    if (s1[index] != s2[index])
+      return s1[index] - s2[index];
   }
   return 0;
 }
@@ -44,11 +49,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-    char* cs = (char *)s; // 一个字节一个字节赋值
-    for (int i = 0; i < n; i++) {
-        cs[i] = c;
-    }
-    return s;
+  char *cs = (char *)s; // 一个字节一个字节赋值
+  for (int i = 0; i < n; i++) {
+    cs[i] = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -56,14 +61,20 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *ci = (char *)out; // 一个字节一个字节赋值
+  char *co = (char *)in;
+  for (int i = 0; i < n; i++) {
+    ci[i] = co[i];
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  char* cs1 = (char *)s1;
-  char* cs2 = (char *)s2;
+  char *cs1 = (char *)s1;
+  char *cs2 = (char *)s2;
   for (int i = 0; i < n; i++) {
-     if (cs1[i] != cs2[i]) return cs1[i] - cs2[i];
+    if (cs1[i] != cs2[i])
+      return cs1[i] - cs2[i];
   }
   return 0;
 }
