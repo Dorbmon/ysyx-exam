01485c6d6df10ce9c292274a8023a949525534dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 10:21:55 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:21:55 up 13:50,  0 users,  load average: 0.02, 0.09, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,15 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  while (s [len] != '\0') len++;
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char* res = dst;
+  while ((*(dst) = *(src++)) != '\0');
+  return res;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +21,22 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (dst [i] != '\0') ++ i;
+  while (*src != '\0') dst [i++] = *(src++);
+  dst [i] = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t l1 = strlen(s1),l2 = strlen(s2);
+  if (l1 != l2) {
+    return l1 - l2;
+  }
+  for (size_t index = 0;index < l1;++ index) {
+    if (s1 [index] != s2 [index]) return s1 [index] - s2 [index];
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +44,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+    char* cs = (char *)s; // 一个字节一个字节赋值
+    for (int i = 0; i < n; i++) {
+        cs[i] = c;
+    }
+    return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +60,12 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  char* cs1 = (char *)s1;
+  char* cs2 = (char *)s2;
+  for (int i = 0; i < n; i++) {
+     if (cs1[i] != cs2[i]) return cs1[i] - cs2[i];
+  }
+  return 0;
 }
 
 #endif
