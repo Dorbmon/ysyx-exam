2b4b0623c60d55793c7bdce99575263a6a29ae9a
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 11:16:42 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:42 up 14:45,  0 users,  load average: 0.30, 0.31, 0.33

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,9 +5,9 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t len = 0;
-  while (*(s + len) != '\0') len++;
-  return len;
+  for (size_t i = 0;;i ++) {
+    if (s [i] == '\0') return i;
+  }
 }
 
 char *strcpy(char *dst, const char *src) {
