f96a11c4034058bc84397a28add0ff9279baddc5
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-27 00:11:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:11:54 up 50 min,  0 users,  load average: 0.10, 0.21, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -63,12 +63,8 @@ int sprintf(char *str, const char *fmt, ...) {
   char c;
   char *s;
   int n;
-  int index = 0;
-  int ret = 2;
   char buf[65];
   char digit[16];
-  int num = 0;
-  int len = 0;
   memset(buf, 0, sizeof(buf));
   memset(digit, 0, sizeof(digit));
   va_list ap;
@@ -116,83 +112,6 @@ int sprintf(char *str, const char *fmt, ...) {
         *(str++) = '%';
         break;
       }
-      case '0': /*位不足的左补0*/
-      {
-        index = 0;
-        num = 0;
-        memset(digit, 0, sizeof(digit));
-
-        while (1) {
-          fmt++;
-          ret = isDigit(*fmt);
-          if (ret == 1) //是数字
-          {
-            digit[index] = *fmt;
-            index++;
-          } else {
-            num = atoi(digit);
-            break;
-          }
-        }
-        switch (*fmt) {
-        case 'd': /*整型*/
-        {
-          n = va_arg(ap, int);
-          if (n < 0) {
-            *str = '-';
-            str++;
-            n = -n;
-          }
-          itoa(n, buf);
-          len = strlen(buf);
-          if (len >= num) {
-            memcpy(str, buf, strlen(buf));
-            str += strlen(buf);
-          } else {
-            memset(str, '0', num - len);
-            str += num - len;
-            memcpy(str, buf, strlen(buf));
-            str += strlen(buf);
-          }
-          break;
-        }
-        case 'x': /*16进制*/
-        {
-          n = va_arg(ap, int);
-          xtoa(n, buf);
-          len = strlen(buf);
-          if (len >= num) {
-            memcpy(str, buf, len);
-            str += len;
-          } else {
-            memset(str, '0', num - len);
-            str += num - len;
-            memcpy(str, buf, len);
-            str += len;
-          }
-          break;
-        }
-        case 's': /*字符串*/
-        {
-          s = va_arg(ap, char *);
-          len = strlen(s);
-          if (len >= num) {
-            memcpy(str, s, strlen(s));
-            str += strlen(s);
-          } else {
-            memset(str, '0', num - len);
-            str += num - len;
-            memcpy(str, s, strlen(s));
-            str += strlen(s);
-          }
-          break;
-        }
-        default:
-          break;
-        }
-      }
-      default:
-        break;
       }
     } else {
       *(str++) = *fmt;
