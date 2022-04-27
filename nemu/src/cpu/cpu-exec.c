8475cc6aab1563fbbbca79b9b0d61c3087a45c82
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-27 13:14:40 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:14:40 up 13:53,  0 users,  load average: 0.03, 0.05, 0.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -22,6 +22,7 @@ void device_update();
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   //if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+  printf("55wa5w");
   log_write("%s\n", _this->logbuf);
 #endif
 #ifdef CONFIG_WATCHPOINT
