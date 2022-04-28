d50ed6787e25ea0b5b0148e3038805ad956c394e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 10:26:51 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:26:51 up 11:05,  0 users,  load average: 0.00, 0.05, 0.07

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -4,9 +4,9 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0;i < 32;++ i) {
-    printf("%lx %lx\n", ref_r->gpr [i], cpu.gpr [i]);
     if (ref_r->gpr [i] != cpu.gpr [i]) return false;
   }
+  printf("%lx %lx\n", ref_r->pc, pc);
   return ref_r->pc == pc;
 }
 
