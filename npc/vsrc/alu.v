c57346eb0b9c9ddd03a4cb84a46c7e7fe522028e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 21:07:06 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:07:06 up 1 day, 21:45,  0 users,  load average: 0.02, 0.06, 0.14

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -20,8 +20,8 @@ ysyx_22041207_ext ext(inst, immI, immS, immB, immU, immJ, funct7, funct3);
 ysyx_22041207_MuxKeyWithDefault #(5, 7, 129) rmux ({wen, wdata, newPcValue}, inst[6:0], 129'b0, {
     7'b0010011, {1'b1, rs1 + immI, 64'b0},   // addi
     7'b0010111, {1'b0, pc + immU,  64'b0},   // auipc
-    7'b0110111, {1'b1, immU, 64'b0},
-    7'b1101111, {1'b1, pc + 64'o4, pc + immJ},   //jal
+    7'b0110111, {1'b1, immU, 64'b0},        // lui
+    7'b1101111, {1'b1, pc + 64'b100, pc + immJ},   //jal
     7'b1100111, (funct3==0)?{1'b1, pc + 64'b100, rs1 + immJ}:129'b0  //jalr
 });
 endmodule
