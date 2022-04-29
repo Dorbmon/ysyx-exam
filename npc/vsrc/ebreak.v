aacffcc38b819e5c9b2b77f169eec450fb9294cb
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 20:53:51 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:53:51 up 1 day, 21:32,  0 users,  load average: 0.07, 0.34, 0.29

--- npc/vsrc/ebreak.v
+++ npc/vsrc/ebreak.v
@@ -12,7 +12,7 @@ wire [2:0]  funct3;
 ysyx_22041207_ext ext(inst, immI, immS, immB, immU, immJ, funct7, funct3); 
 always @(*) begin
     $display("inst:%b", inst);
-    if (inst[6:0] == 7'b1110011 && funct3 == 0) begin
+    if (inst[6:0] == 7'b1110011 && funct3 == 0 && immI == 64'h1) begin
         ebreak();
     end
 end
