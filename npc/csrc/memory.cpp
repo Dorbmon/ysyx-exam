9cf0b20a780015ef3254d11fedb0ab240b4768ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 20:54:43 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:54:43 up 1 day, 21:33,  0 users,  load average: 0.03, 0.28, 0.27

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -38,8 +38,8 @@ word_t pmem_read(paddr_t addr, int len) {
 static char *img_file = NULL;
 void initMemory(int argc,char** argv) {
   assert(argc == 2);
-  //char *img_file = argv[1];
-  char* img_file = "/home/dorbmon/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin";
+  char *img_file = argv[1];
+  //char* img_file = "/home/dorbmon/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv64-nemu.bin";
   printf("load img file:%s\n", img_file);
   FILE *fp = fopen(img_file, "rb");
   assert(fp != NULL);
