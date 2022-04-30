e3c0e2e3258a298673ef7d526b1b44fc8951cc0e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-30 21:07:13 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:07:13 up  7:59,  0 users,  load average: 0.02, 0.24, 0.53

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <nvboard.h>
 #include <Vysyx_22041207_top.h>
+#include <sys/types.h>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include <iostream>
@@ -8,6 +9,9 @@
 #include "svdpi.h"
 #include "Vysyx_22041207_top__Dpi.h"
 #include "verilated_dpi.h"
+#include <readline/history.h>
+#include <readline/readline.h>
+#define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
 double sc_time_stamp() { return 0; }
 const std::unique_ptr<VerilatedContext> contextp = std::make_unique<VerilatedContext>();
 Vysyx_22041207_top* top = new Vysyx_22041207_top{contextp.get(), "TOP"};
@@ -19,6 +23,51 @@ void ebreak() {
 uint64_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
+}
+static void runN(uint64_t n) {
+  uint64_t count = 0;
+  n <<= 1;
+  while (!contextp->gotFinish() && !sebreak && count < n) {
+    //contextp->timeInc(1);
+    //vcd->dump(time);
+    count ++;
+    if (count & 1) {
+      printf("pc:%lx\n", top->pc);
+    }
+    top->clk = ~top->clk;
+    top->inst = pmem_read(top->pc, 4);
+    //std::cout << "Here" << std::endl;
+    top->eval();
+  }
+}
+static int simulate(char *args) {
+  uint64_t N = 1;
+  if (args != NULL) {
+    N = atoi(args);
+  }
+  runN(N);
+  return 0;
+}
+static struct {
+  const char *name;
+  const char *description;
+  int (*handler)(char *);
+} cmd_table[] = {
+    {"si", "Run N lines of code", simulate}
+};
+
+#define NR_CMD ARRLEN(cmd_table)
+static char *rl_gets() {
+  static char *line_read = NULL;
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+  line_read = readline("(npc) ");
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+  return line_read;
 }
 int main(int argc, char **argv, char **env) {
   //nvboard_bind_all_pins(top);
@@ -36,20 +85,32 @@ int main(int argc, char **argv, char **env) {
   top->eval();
   top->rst = 0;
   top->clk = 0;
-  while (!contextp->gotFinish() && !sebreak && time < 50) {
-    //contextp->timeInc(1);
-    //vcd->dump(time);
-    time ++;
-    if (time & 1) {
-      printf("pc:%lx\n", top->pc);
+  for (char *str; (str = rl_gets()) != NULL;) {
+    char *str_end = str + strlen(str);
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) {
+      continue;
     }
-    top->clk = ~top->clk;
-    top->inst = pmem_read(top->pc, 4);
-    //std::cout << "Here" << std::endl;
-    top->eval();
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
 
-    //nvboard_update();printf("gpr[%d] = 0x%lx\n", i, cpu_gpr[i]);
+    int i;
+    for (i = 0; i < NR_CMD; i++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) {
+          return 0;
+        }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) {
+      printf("Unknown command '%s'\n", cmd);
+    }
   }
+  
   if (sebreak) {
     if (cpu_gpr [10] == 0) {
       printf("HIT GOOD TRAP.\n");
