aad0d3a941110c471b9c6bbdcdcc1b4d22f467a8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 12:45:59 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:45:59 up 51 min,  0 users,  load average: 0.42, 0.22, 0.14

--- npc/csrc/itrace.cpp
+++ npc/csrc/itrace.cpp
@@ -1,12 +1,19 @@
 #include "itrace.h"
 
-void loadINST(uint8_t inst, uint32_t pc) {
+void loadINST(uint8_t rinst, uint32_t pc) {
   char logbuf [128];
   char *p = logbuf;
-  char *begin = logbuf;
+  char* begin = logbuf;
   p += snprintf(p, sizeof(logbuf), FMT_WORD ":", pc);
+  uint8_t *inst = (uint8_t *)&rinst;
+  for (int i = 4 - 1; i >= 0; i --) {
+    p += snprintf(p, 4, " %02x", inst[i]);
+  }
+  int space_len = 1;
+  memset(p, ' ', space_len);
+  p += space_len;
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, logbuf + sizeof(logbuf) - p,
-              pc,
-              (uint8_t *)&inst, 4);
-  printf("%s\n", logbuf);
+      pc, (uint8_t *)&rinst, 4);
+  printf("%s \n", logbuf);
 }
