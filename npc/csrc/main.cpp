0284527f9310e642ab7c2193600c1a533e5475a9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 20:58:35 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:58:35 up  9:04,  0 users,  load average: 0.01, 0.12, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -154,7 +154,7 @@ int main(int argc, char **argv, char **env) {
   top->rst = 1;
   top->eval();
   top->rst = 0;
-  top->clk = 1;
+  top->clk = 0;
   top->eval();
   for (char *str; (str = rl_gets()) != NULL;) {
     char *str_end = str + strlen(str);
