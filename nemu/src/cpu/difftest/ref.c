5766d85de6a9081ee0e4e8cb8bbb01c386c5da7f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 20:21:32 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:21:32 up  8:27,  0 users,  load average: 0.46, 0.32, 0.20

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -7,6 +7,7 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction) {
     for (size_t i = 0;i < n;++ i) {
       //*(uint8_t*)(buf+i) = paddr_read(addr, 1);
+      printf("loaded..............\n");
       paddr_write(addr + i, 1,*((uint8_t*)buf + i));
     }
   } else {
