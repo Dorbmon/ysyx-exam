edb24f1595e2e7ed2176fa9a787c1240a6cc7814
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 20:23:02 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:02 up  8:28,  0 users,  load average: 0.34, 0.30, 0.20

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -14,7 +14,6 @@ static const uint32_t img [] = {
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
-  printf("inittttttd\n");
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
 }
