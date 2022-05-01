fa7480b524280a41229e83cc8eb647d6754170c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 20:14:35 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:14:35 up  8:20,  0 users,  load average: 0.07, 0.08, 0.09

--- npc/csrc/diff.cpp
+++ npc/csrc/diff.cpp
@@ -57,7 +57,6 @@ static void checkregs(diff_context_t *ref, uint64_t pc) {
 }
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
-  return ;
   diff_context_t ref_r;
 
   if (skip_dut_nr_inst > 0) {   // 当前这条指令dut不执行
