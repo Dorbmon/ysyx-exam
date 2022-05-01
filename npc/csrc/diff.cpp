92bfda1ce85d8f128117a5f20a74351090807108
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 20:39:10 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:39:10 up  8:44,  0 users,  load average: 0.29, 0.19, 0.18

--- npc/csrc/diff.cpp
+++ npc/csrc/diff.cpp
@@ -42,8 +42,7 @@ bool isa_difftest_checkregs(diff_context_t *ref_r, uint64_t pc) {
   for (int i = 0;i < 32;++ i) {
     if (ref_r->gpr [i] != cpu.gpr [i]) return false;
   }
-  printf("%lx %lx\n",ref_r->pc, cpu.pc);
-  return ref_r->pc == cpu.pc;
+  return ref_r->pc == pc;
 }
 static void checkregs(diff_context_t *ref, uint64_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
