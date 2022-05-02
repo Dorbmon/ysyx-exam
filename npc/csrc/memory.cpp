0d798159bd05bce65368b8974ea48f2deeaf5d91
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 22:20:25 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:20:25 up 10:27,  0 users,  load average: 0.15, 0.07, 0.01

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -61,6 +61,7 @@ void initMemory(const char *img_file) {
 extern "C" void pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   if ((raddr & ~0x7ull) < CONFIG_MBASE) return ;
+  printf("should be:%lx\n", pmem_read (80000173, 1));
   *rdata = pmem_read(raddr & ~0x7ull, 8); //111
   printf("read:%llx, but:%llx", raddr, raddr & ~0x7ull);
 }
