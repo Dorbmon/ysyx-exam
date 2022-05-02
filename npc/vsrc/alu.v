6bbbb149e0b1e9bd7fb3fb9e04d8d854d74d07d8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 22:59:41 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:59:41 up 11:06,  0 users,  load average: 0.00, 0.01, 0.00

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -53,7 +53,7 @@ ysyx_22041207_MuxKeyWithDefault #(9, 7, 65) rmux ({wen, wdata}, opCode, 65'b0, {
                 (funct3 == 3'b100)?{1'b1, rs1 ^ immI}:    //xori
                 (funct3 == 3'b101 && funct7 == 7'b0000000)?{1'b1, rs1 >> immI[4:0]}:  //srli
                 (funct3 == 3'b101 && funct7 == 7'b0100000)?{1'b1, rs1 >> immI[4:0]}:  //srai
-                (funct3 == 3'b001 && funct7[7:1] == 6'b000000)?{1'b1, rs1 << immI[4:0]}:  //slli
+                (funct3 == 3'b001 && funct7[6:1] == 6'b000000)?{1'b1, rs1 << immI[4:0]}:  //slli
                 65'b0,
     7'b0011011, (funct3 == 3'b101 && funct7 == 7'b0000000)?{1'b1, `SEXT(rs1 >> immI, 32)}:  //srliw
                 (funct3 == 3'b101 && funct7 == 7'b0100000)?{1'b1, `SEXT($signed(rs1[31:0]) >>> $signed(immI[5:0]), 32)}: //sraiw
