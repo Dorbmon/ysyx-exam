7a2344d74d57b25198654b028714f5741359f3fb
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-03 12:02:29 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:02:29 up  2:15,  0 users,  load average: 0.12, 0.47, 0.37

--- abstract-machine/scripts/riscv64-nemu.mk
+++ abstract-machine/scripts/riscv64-nemu.mk
@@ -6,4 +6,4 @@ AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
            riscv/nemu/trap.S \
            riscv/nemu/vme.c
-CFLAGS += -DMAINARGS=\"$(mainargs)\"
+CFLAGS += -DMAINARGS=\"test$(mainargs)\"
