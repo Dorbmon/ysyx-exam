b5450258582ac9add1378ca380d3222708653260
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-03 13:58:52 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:58:52 up  4:11,  0 users,  load average: 0.12, 0.21, 0.09

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -23,7 +23,6 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  printf("enter mmio\n");
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
