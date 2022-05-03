2810f0acb88820cec229f93f6834b126ecb0b574
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-03 12:04:55 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:04:55 up  2:17,  0 users,  load average: 0.66, 0.45, 0.37

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -6,7 +6,7 @@ int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
-#define MAINARGS "test"
+#define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
