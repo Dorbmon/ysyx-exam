51c69c0c0d31209416ecb60476eae86f7d9b1d14
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-03 16:17:07 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:17:07 up  6:29,  0 users,  load average: 0.36, 0.54, 0.56

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -42,6 +42,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
+  MUXDEF(CONFIG_DTRACE, printf("device[%s] read at 0x%x\n", map->name, addr), );
   word_t ret = host_read(map->space + offset, len);
   return ret;
 }
@@ -51,5 +52,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+  MUXDEF(CONFIG_DTRACE, printf("device[%s] write at 0x%x\n", map->name, addr), );
   invoke_callback(map->callback, offset, len, true);
 }
