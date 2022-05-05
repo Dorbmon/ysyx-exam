04f832e870d570139b408c5f78e6c8e76f843049
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 19:41:44 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:41:44 up  3:29,  0 users,  load average: 0.19, 0.12, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,16 +1,66 @@
 #include <proc.h>
 #include <elf.h>
-
-#ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
-#else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
-#endif
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  Elf64_Ehdr elf_head;
+	int a;
+	a = ramdisk_read(&elf_head, 0, sizeof(Elf64_Ehdr)); 
+  if (0 == a) {
+		printf("fail to read head\n");
+		assert(0);
+	}
+
+	// 判断elf文件类型
+	if (elf_head.e_ident[0] != 0x7F ||
+		elf_head.e_ident[1] != 'E' ||
+		elf_head.e_ident[2] != 'L' ||
+		elf_head.e_ident[3] != 'F') {
+		printf("Not a ELF file\n");
+		assert(0);
+	}
+  size_t roffset = 0;
+	Elf64_Shdr *shdr = (Elf64_Shdr*)malloc(sizeof(Elf64_Shdr) * elf_head.e_shnum);
+	if (NULL == shdr) {
+		printf("shdr malloc failed\n");
+		assert(0);
+	}
+  roffset = 0;
+	// a = fseek(elf_fp, elf_head.e_shoff, SEEK_SET);
+	// if (0 != a)
+	// {
+	// 	printf("\nfaile to fseek\n");
+	// 	exit(0);
+	// }
+  roffset = elf_head.e_phoff;
+	a = ramdisk_read(shdr, elf_head.e_shoff, sizeof(Elf64_Shdr) * elf_head.e_shnum);
+	if (0 == a) {
+		printf("\nfail to read section\n");
+		return 0;
+	}
+  roffset += a;
+  roffset = shdr[elf_head.e_shstrndx].sh_offset;
+	//fseek(elf_fp, shdr[elf_head.e_shstrndx].sh_offset, SEEK_SET);
+	char shstrtab[shdr[elf_head.e_shstrndx].sh_size];
+	// 读取内容
+	a = ramdisk_read(shstrtab, roffset, shdr[elf_head.e_shstrndx].sh_size);
+	if (0 == a) {
+		printf("\nfaile to read\n");
+    assert(0);
+	}
+  roffset = elf_head.e_phoff;
+  uint8_t buf [50000];
+  for (int i = 0;i < elf_head.e_phnum;++ i) {
+    Elf64_Phdr tmp;
+    ramdisk_read(&tmp, roffset, sizeof(Elf64_Phdr));
+    if (tmp.p_type == PT_LOAD) {
+      // 需要加载
+      ramdisk_read(buf, tmp.p_offset, tmp.p_memsz);
+      memcpy((uint8_t*)tmp.p_vaddr, buf, tmp.p_memsz);
+      memset((uint8_t*)tmp.p_vaddr + tmp.p_filesz, 0, tmp.p_memsz - tmp.p_filesz);
+    }
+  }
+  free(shdr);
   return 0;
 }
 
