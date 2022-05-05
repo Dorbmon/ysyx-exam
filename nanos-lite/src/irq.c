02993aab643f3c6ad6dd386868dc146338ec9305
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 21:06:28 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:06:28 up  4:54,  0 users,  load average: 0.14, 0.07, 0.04

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include <common.h>
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
+  printf("rrrr:%d\n", EVENT_SYSCALL);
   switch (e.event) {
     case EVENT_SYSCALL: do_syscall(c); break;
     case EVENT_YIELD: printf("catched yield...\n"); break;
