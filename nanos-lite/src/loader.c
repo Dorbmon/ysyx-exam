1c3444695312785079eafcff02401e437b04d400
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 20:53:50 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:53:50 up  4:41,  0 users,  load average: 0.59, 0.17, 0.05

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,8 +25,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     Elf64_Phdr tmp;
     roffset += ramdisk_read(&tmp, roffset, sizeof(Elf64_Phdr));
     if (tmp.p_type == PT_LOAD) {
-      // 需要加载
-      printf("vaddr: %x\n", tmp.p_vaddr);
       ramdisk_read((uint8_t*)tmp.p_vaddr, tmp.p_offset, tmp.p_filesz);
       memset((uint8_t*)tmp.p_vaddr + tmp.p_filesz, 0, tmp.p_memsz - tmp.p_filesz);
     }
