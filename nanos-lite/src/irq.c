c5c185f16fef72c63effa556df7348d69d2074fc
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 18:02:47 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:02:47 up  1:50,  0 users,  load average: 0.56, 0.26, 0.12

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
+  printf("rrr:%d\n", EVENT_YIELD);
   switch (e.event) {
     case EVENT_YIELD: printf("catched yield...\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
