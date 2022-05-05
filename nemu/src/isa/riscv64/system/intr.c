2a6bc09d01eae550bf97b4fe0f1ef326cfcc78b3
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 17:30:17 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:30:17 up  1:17,  0 users,  load average: 0.24, 0.11, 0.07

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -4,6 +4,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  printf("called intr>>>\n");
   cpu.csrM [0x341] = epc;
   cpu.csrM [0x342] = NO;
   return cpu.csrM [0x305];
