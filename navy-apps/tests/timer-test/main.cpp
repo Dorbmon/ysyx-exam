238139a65693c64f2eea9e24628c359313538ff7
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-10 11:15:20 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:15:20 up 1 day, 12:49,  0 users,  load average: 0.69, 0.35, 0.15

--- navy-apps/tests/timer-test/main.cpp
+++ navy-apps/tests/timer-test/main.cpp
@@ -8,7 +8,6 @@ int main() {
   last = NDL_GetTicks();
   while (1) {
     t = NDL_GetTicks();
-    printf("%ld..\n", t);
     if (t - last > 500000) {
       printf("after 0.5 second...\n");
       last = t;
