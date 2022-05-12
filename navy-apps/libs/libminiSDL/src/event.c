f78f7769fd039d96da0d5fc3ea53c0772aa2fbed
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-12 19:45:32 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:45:32 up 10 min,  0 users,  load average: 1.53, 1.05, 0.52

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <sdl-event.h>
 #include <stdlib.h>
+#include <string.h>
 #define keyname(k) #k,
 
 static const char *keyname[] = {
@@ -12,16 +13,21 @@ static const char *keyname[] = {
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
-
+int keyToIndex(char* key) {
+  for (int i = 0;i < sizeof(keyname) / sizeof(char*);++ i) {
+    if (strcmp(key, keyname[i]) == 0) return i;
+  }
+  return 0;
+}
 int SDL_PollEvent(SDL_Event *ev) {
   static char buf[64];
   if (NDL_PollEvent(buf, sizeof(buf))) {
     if (buf [1] == 'd') { //kd
       ev->type = SDL_KEYDOWN;
-      ev->key.keysym.sym = atoi(buf + 3);
+      ev->key.keysym.sym = keyToIndex(buf + 3);
     } else {
       ev->type = SDL_KEYUP;
-      ev->key.keysym.sym = atoi(buf + 3);
+      ev->key.keysym.sym = keyToIndex(buf + 3);
     }
     return 1;
   } else {
