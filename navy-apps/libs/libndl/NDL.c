47203c01abf9af2513d68eefa4e062c9f44b5bca
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-12 21:08:17 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:08:17 up  1:33,  0 users,  load average: 0.24, 0.18, 0.13

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -46,12 +46,13 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   FILE* fd = fopen("/dev/fb", "w");
   for (int i = y;i < y + h && i < real_h;++ i) {
-    printf("%d\n", i);
+    printf("%d\n", (i * real_w + x) / real_w);
     fseek(fd, i * real_w + x, SEEK_SET);
     size_t len = w * sizeof(uint32_t);
     if (x + w > screen_w) {
       //len = (screen_w - x) * sizeof(uint32_t);
     }
+    
     fwrite(pixels + (i - y) * w, len, 1, fd);
     printf("out\n");
   }
