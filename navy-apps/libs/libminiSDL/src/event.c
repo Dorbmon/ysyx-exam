f2a021ccb8783635f26c3f14c4117c44890f0bb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-12 19:48:33 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:48:33 up 13 min,  0 users,  load average: 0.60, 0.91, 0.57

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,6 +15,7 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 int keyToIndex(char* key) {
   for (int i = 0;i < sizeof(keyname) / sizeof(char*);++ i) {
+    printf ("%s %s\n", key, keyname[i]);
     if (strcmp(key, keyname[i]) == 0) return i;
   }
   return 0;
@@ -29,7 +30,6 @@ int SDL_PollEvent(SDL_Event *ev) {
       ev->type = SDL_KEYUP;
       ev->key.keysym.sym = keyToIndex(buf + 3);
     }
-    printf("set key:%d\n",ev->key.keysym.sym );
     return 1;
   } else {
     return 0;
