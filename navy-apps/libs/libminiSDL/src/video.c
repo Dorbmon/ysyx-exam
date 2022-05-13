daf681313edb9996d085ae48a630604fd86410f7
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 12:52:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:52:01 up  1:35,  0 users,  load average: 0.28, 0.14, 0.09

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -17,7 +17,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   if (dstrect == NULL) dstrect = &full;
   for (int i = 0;i < srcrect->h;++ i) {
     for (int j = 0;j < srcrect->w;++ j) {
-      printf("%p %p\n", (uint32_t*)dst->pixels + (i + dstrect->y) * dst->w + j + dstrect->x, (uint32_t*)src->pixels + (i + srcrect->y) * src->w + j + srcrect->x);
+      //printf("%p %p\n", (uint32_t*)dst->pixels + (i + dstrect->y) * dst->w + j + dstrect->x, (uint32_t*)src->pixels + (i + srcrect->y) * src->w + j + srcrect->x);
      *((uint32_t*)dst->pixels + (i + dstrect->y) * dst->w + j + dstrect->x) = *((uint32_t*)src->pixels + (i + srcrect->y) * src->w + j + srcrect->x);
     }
   }
