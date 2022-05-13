34fa7391b626e040c8d727259c70757f7c3ce1c6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 23:53:50 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:53:50 up 12:37,  0 users,  load average: 0.05, 0.07, 0.08

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -173,7 +173,7 @@ void SDL_SoftStretch(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
         int scaleX = fixedpt_toint(fixedpt_mul(fixedpt_div(fixedpt_fromint(j), fixedpt_fromint(dstrect->w)), srcrect->w));
         if (src->format->BitsPerPixel == 8) {
           printf("%d %d %d %d\n", i, dstrect->h, scaleY, srcrect->h);
-          printf("%d\n", fixedpt_div(fixedpt_fromint(i), fixedpt_fromint(dstrect->h)));
+          printf("%d\n", fixedpt_mul(fixedpt_div(fixedpt_fromint(i), fixedpt_fromint(dstrect->h)), srcrect->h));
           dst->pixels [(i + dstrect->y) * dst->w + dstrect->x + j] = src->pixels[(scaleY + srcrect->y) * src->w + srcrect->x + scaleX];
         } else {
           ((uint32_t*)dst->pixels) [(i + dstrect->y) * dst->w + dstrect->x + j] = ((uint32_t*)src->pixels)[(scaleY + srcrect->y) * src->w + srcrect->x + scaleX];
