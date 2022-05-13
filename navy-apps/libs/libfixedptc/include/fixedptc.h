0db594ec5b1432866862dadea52cbdd912987bef
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 23:35:26 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:35:26 up 12:18,  0 users,  load average: 0.34, 0.13, 0.11

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -143,7 +143,7 @@ static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	return ((A / B) << FIXEDPT_FBITS) + (A & FIXEDPT_FMASK) / (B & FIXEDPT_FMASK);
+	return ((A / B) << FIXEDPT_FBITS);
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
