65c6e06173c62c2ff5d8ae076dfacd5005d3bce6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 17:27:59 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:59 up 17:08,  0 users,  load average: 0.33, 0.39, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,6 +37,7 @@ void init_proc() {
 // 返回新的上下文
 Context* schedule(Context *prev) {
   // 先保存当前的上下文
+  printf("called sch\n");
   current->cp = prev;
   current = &pcb[0]; // 选择第一个
   return current->cp;
