3983cbfde46f2897b1da9db519683da703964ca7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 11:55:37 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:55:37 up 1 day, 10:01,  0 users,  load average: 0.08, 0.04, 0.01

--- npc/vsrc/getInstType.v
+++ npc/vsrc/getInstType.v
@@ -10,7 +10,7 @@ assign instType = (opCode == 7'b0110011) ? `R_Type :
     (opCode == 7'b0100011 ? `S_Type : 
     (opCode == 7'b1100011 ? `B_Type : 
     (opCode == 7'b1101111 ? `J_Type : 
-    (opCode == 7'b0110111 ? `U_Type : 3'b0
+    ((opCode == 7'b0110111 || opCode == 7'b0010111) ? `U_Type : 3'b0
     )))));
 
 endmodule
