4c1f23c47e1ac18438389f7018d9b0f72cbfbfcd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 11:01:02 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:01:02 up 10 min,  0 users,  load average: 0.01, 0.03, 0.00

--- npc/vsrc/sext.v
+++ npc/vsrc/sext.v
@@ -16,7 +16,6 @@ module ysyx_22041207_SEXT(
                 case (funct3)
                     3'b110: begin
                         // 0扩展
-                        $display("zero");
                         imm = {{(51){1'b0}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
                     end
                     3'b111: begin
