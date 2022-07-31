c7c311014bcd5b4b56fc4245c1db5b17cd467b4f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 11:05:52 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:05:52 up 1 day, 15 min,  0 users,  load average: 0.16, 0.08, 0.09

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -8,7 +8,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   printf("intr::::NO:%ld\n", NO);
   csrM [0x341] = epc; //mepc
   //csrM [0x342] = NO;  //mcause
-  csrM [0x342] = 0x123;
+  csrM [0x342] = 0xb;
   uint64_t mstatus = csrM[0x300];
   mpie = mstatus & (1ull << 3);
   csrM[0x300] = mstatus & (~(1ull << 3));
