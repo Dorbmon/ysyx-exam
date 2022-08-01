1dbd8906b4fd26393593e5fdf40bdb3950418dbc
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 23:47:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:47:54 up  2:44,  0 users,  load average: 0.07, 0.16, 0.11

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -45,7 +45,7 @@ uintptr_t loader(PCB *pcb, const char *filename) {
           memset(pg, 0, PGSIZE);
         }
         #else
-        fs_read(fd, (uint8_t*)tmp.p_vaddr , tmp.p_memsz);
+        fs_read(fd, (uint8_t*)tmp.p_vaddr , tmp.p_filesz);
         memset((uint8_t*)tmp.p_vaddr + tmp.p_filesz, 0, tmp.p_memsz - tmp.p_filesz);
         #endif
       }
