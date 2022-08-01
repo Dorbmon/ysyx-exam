bf415ecbcec50a5aa08fa1daa48a8321f7f9476c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 21:20:11 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:20:11 up 16 min,  0 users,  load average: 0.32, 0.27, 0.15

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -44,14 +44,14 @@ void hello_fun(void *arg) {
 }
 void naive_uload(PCB *pcb, const char *filename);
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, "f1");
-  context_kload(&pcb[1], hello_fun, "f2");
+  //context_kload(&pcb[0], hello_fun, "f1");
+  //context_kload(&pcb[1], hello_fun, "f2");
 
   switch_boot_pcb();
   Log("Initializing processes...");
 
   // load program here
-  //context_uload(&pcb[0], "/bin/dummy");
+  context_uload(&pcb[0], "/bin/dummy");
 }
 // 返回新的上下文
 Context* schedule(Context *prev) {
