10da85e3fd16ca73bc773b71e1969289d24fe75e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 09:23:32 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:23:32 up 22 min,  0 users,  load average: 0.27, 0.28, 0.31

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -9,11 +9,11 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  //printf("start to run dummy....\n");
+  printf("start to run dummy....\n");
   //putchar('R');
   //while (1);
-  char t[] = "hello world";
-  _syscall_(SYS_write, 1, (uintptr_t)t, sizeof(t));
+  //char t[] = "hello world";
+  //_syscall_(SYS_write, 1, (uintptr_t)t, sizeof(t));
   while (1);
   //return 0;
   //return _syscall_(SYS_exit, 0, 0, 0);
