96a09fd46624d2d90f036ec70d118b05761b4d47
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-02 09:50:41 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:50:41 up 49 min,  0 users,  load average: 0.08, 0.06, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,6 +35,7 @@ void context_uload(PCB *pcb, const char *filename) {
   for (int i = 0;i < 8;++ i) {
     map(&pcb->as, pcb->as.area.end - PGSIZE * i, kstack.end - PGSIZE * i, 0);
   }
+  pcb->max_brk = (uintptr_t)pcb->as.area.end;
   #endif
   //((void(*)())entry) ();
 }
