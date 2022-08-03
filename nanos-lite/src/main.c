d4d3541b9849817b37637b8e17988c5fea039cf5
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 11:38:11 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:38:11 up 12:01,  0 users,  load average: 0.08, 0.26, 0.18

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -8,8 +8,8 @@ void init_fs(void);
 void init_proc(void);
 
 int main() {
-  extern const char logo[];
-  printf("%s", logo);
+  //extern const char logo[];
+  printf("%s", "hello world...");
   //printf(logo);
   while(1);
   Log("'Hello World!' from Nanos-lite");
