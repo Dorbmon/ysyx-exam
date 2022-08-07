eeb0858ad6e7cd7cb94a6f678efbd27c17548ef5
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 13:33:33 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:33:33 up 15:40,  0 users,  load average: 0.00, 0.05, 0.04

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -4,7 +4,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  //uptime->us = 0;
+  uptime->us = *(uint64_t*)(0xa0000048);//((uint64_t)inl(0xa0000048 + 0x4) << 32) + (uint64_t)inl(0xa0000048);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
