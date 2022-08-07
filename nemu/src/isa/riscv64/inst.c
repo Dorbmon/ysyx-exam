d656949a76fbe44d11a3d743082bd2b91df2ff7e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 09:29:09 +0800

    > compile NEMU ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:29:09 up 11:36,  0 users,  load average: 0.12, 0.10, 0.34

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -62,9 +62,9 @@ int depth = 0;
 void rjal(Decode *s, word_t dest,word_t src1,word_t src2) {
   //printf("jal: %lx, %lx\n",s->pc , s->pc + src1);
   R(dest) = s->pc + 4; s->dnpc = s->pc + src1;
-  if (strcmp(getBelongFunction(s->dnpc), getBelongFunction(s->pc)) != 0) {
+  //if (strcmp(getBelongFunction(s->dnpc), getBelongFunction(s->pc)) != 0) {
     //printf("%lx:%*scall [%s@%lx]\n",s->pc,(depth++) * 2, "", getBelongFunction(s->dnpc), s->dnpc);
-  }
+  //}
 }
 void rjalr(Decode *s, word_t dest,word_t src1,word_t src2) {
   //R(dest) = s->pc + 4; s->pc = src1 + src2;
