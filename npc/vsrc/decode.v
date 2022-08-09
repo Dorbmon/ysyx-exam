c1b761b55a035331a4829c84a46b4c406ed3b58b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 14:27:04 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:27:04 up 2 days, 16:34,  0 users,  load average: 0.15, 0.03, 0.01

--- npc/vsrc/decode.v
+++ npc/vsrc/decode.v
@@ -34,6 +34,7 @@ assign funct3 = inst [14:12];
 always @(inst)
 begin
     $display("decode %x %t", inst, $realtime);
+    $monitor($realtime,,"set=",inst);
     memoryWriteMask = 8'b0;
     wMtvec = 1'b0;
     wMepc = 1'b0;
