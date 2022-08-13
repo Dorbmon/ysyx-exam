c149f4564387a6ba9f6de81a2019c6e032ca16c4
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-13 11:05:20 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:05:20 up 20:54,  0 users,  load average: 0.02, 0.02, 0.00

--- npc/vsrc/ex_me.v
+++ npc/vsrc/ex_me.v
@@ -26,17 +26,28 @@ module ysyx_22041207_EX_ME(
 );
 always @(negedge clk) begin
     if (bubble) begin
-        aluRes_o <= aluRes_o;
-        memoryReadWen_o <= memoryReadWen_o;
-        readNum_o <= readNum_o;
-        pc_o <= pc_o;
-        imm_o <= imm_o;
-        wd_sel_o <= wd_sel_o;
-        rs2_o <= rs2_o;
-        memoryWriteMask_o <= memoryWriteMask_o;
-        sext_o <= sext_o;
-        writeRD_o <= writeRD_o;
-        rwaddr_o <= rwaddr_o;
+        // aluRes_o <= aluRes_o;
+        // memoryReadWen_o <= memoryReadWen_o;
+        // readNum_o <= readNum_o;
+        // pc_o <= pc_o;
+        // imm_o <= imm_o;
+        // wd_sel_o <= wd_sel_o;
+        // rs2_o <= rs2_o;
+        // memoryWriteMask_o <= memoryWriteMask_o;
+        // sext_o <= sext_o;
+        // writeRD_o <= writeRD_o;
+        // rwaddr_o <= rwaddr_o;
+        aluRes_o <= 0;
+        memoryReadWen_o <= 0;
+        readNum_o <= 0;
+        pc_o <= 0;
+        imm_o <= 0;
+        wd_sel_o <= 0;
+        rs2_o <= 0;
+        memoryWriteMask_o <= 0;
+        sext_o <= 0;
+        writeRD_o <= 0;
+        rwaddr_o <= 0;
     end
     else begin
         aluRes_o <= aluRes;
