209046b269e22e5e2374928792c9a7aa74817f5c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 12:24:00 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:24:00 up  3:30,  0 users,  load average: 0.04, 0.03, 0.06

--- npc/vsrc/flush.v
+++ npc/vsrc/flush.v
@@ -1,12 +1,13 @@
 module ysyx_22041207_flush (
+    input clk,
     input ex_jal,
     input ex_jalr,
     input ex_branch,
     input [63:0] ex_aluRes,
     output reg flush
 );
-always @(*) begin
+always @(posedge clk) begin
     // 下一个周期需要冲刷掉中间的全部指令
-    flush = ex_jal || ex_jalr || (ex_branch && ex_aluRes == 0);
+    flush <= ex_jal || ex_jalr || (ex_branch && ex_aluRes == 0);
 end
 endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -123,7 +123,7 @@ ysyx_22041207_EX_ME rxEX_ME(clk,
 );
 wire me_jal, me_jalr, me_dbranch;
 wire [4:0] wb_rwaddr;
-ysyx_22041207_flush rx_flush (me_jal, me_jalr, me_dbranch, me_aluRes, flush);
+ysyx_22041207_flush rx_flush (clk, me_jal, me_jalr, me_dbranch, me_aluRes, flush);
 ysyx_22041207_Memory mem(clk, me_memoryReadWen, me_aluRes, me_r2data, me_memoryWriteMask, me_sext, me_readNum, me_memoryReadData);
 ysyx_22041207_ME_WB me_wb(clk, me_aluRes   ,me_pc      ,me_memoryReadData ,me_imm     ,//csrValue,
  0, me_writeBackDataSelect  , me_writeRD , me_rwaddr,
