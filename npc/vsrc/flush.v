58d0bd3edaf1447632e1fb0fa0eae8fc02936cba
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-15 08:38:38 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:38:38 up 45 min,  0 users,  load average: 0.12, 0.05, 0.03

--- npc/vsrc/flush.v
+++ npc/vsrc/flush.v
@@ -4,13 +4,13 @@ module ysyx_22041207_flush (
     input me_jalr,
     input me_branch,
     input [63:0] me_aluRes,
-    output reg flush
+    output  flush
 );
-always @(posedge clk) begin
+//always @(posedge clk) begin
     // 下一个周期需要冲刷掉中间的全部指令
-    flush <= me_jal || me_jalr || (me_branch && me_aluRes == 0);
-    if (flush) begin
-        $display("flush = %d", flush);
-    end
-end
+    assign flush = me_jal || me_jalr || (me_branch && me_aluRes == 0);
+    //if (flush) begin
+    //    $display("flush = %d", flush);
+    //end
+//end
 endmodule
