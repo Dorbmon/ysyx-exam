6e2dd5e5444e47c36f41e0d25c7cd9cf93cb539e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-17 16:54:29 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:54:29 up 30 min,  0 users,  load average: 0.28, 0.18, 0.11

--- npc/vsrc/if.v
+++ npc/vsrc/if.v
@@ -37,7 +37,6 @@ always @(posedge clk) begin
             pc <= {addRes[63:1], 1'b0};
         end
         else if (pc_panic) begin
-            
             pc <= csr_mtvec;
         end if (~pc_delay) begin
             pc <= pc + 4;
