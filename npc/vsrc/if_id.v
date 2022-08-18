3718c0742c329cb5a343bb43b197f6f4a60d35f6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 20:04:45 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:45 up 11:39,  0 users,  load average: 0.08, 0.02, 0.01

--- npc/vsrc/if_id.v
+++ npc/vsrc/if_id.v
@@ -1,15 +1,15 @@
 module ysyx_22041207_if_id (
     input clk,
-    input flush,
+    input bubble,
     input [31:0] inst,
     input [63:0] pc,
     output reg [31:0] inst_o,
     output reg [63:0] pc_o
 );
 always @(negedge clk) begin
-    if (flush) begin
-        inst_o <= 0;
-        pc_o <= 0;
+    if (bubble) begin
+        inst_o <= inst_o;
+        pc_o <= pc_o;
     end else begin
         inst_o <= inst;
         pc_o <= pc;
