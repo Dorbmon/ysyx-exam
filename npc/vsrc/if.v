b783503f19776f73d0b66a7dbc421d3e55e82813
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 19:10:08 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:10:08 up 10:44,  0 users,  load average: 0.11, 0.08, 0.09

--- npc/vsrc/if.v
+++ npc/vsrc/if.v
@@ -23,14 +23,15 @@ end
 ysyx_22041207_read_mem readInst(pc, 1'b1, rawData);
 assign inst = rawData [31:0];  // 这里可能有BUG
 always @(negedge clk) begin
+    if (flush) begin
+        inst_o <= 0;
+        pc_o <= 0;
+    end else
     if (bubble) begin
         inst_o <= inst_o;
         pc_o <= pc_o;
     end
-    else if (flush) begin
-        inst_o <= 0;
-        pc_o <= 0;
-    end else begin
+     else begin
         inst_o <= inst;
         pc_o <= pc;
     end
