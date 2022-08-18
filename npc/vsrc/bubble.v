4d1485a46e5d5820008865c914af806d9e7f218e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 20:23:51 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:51 up 11:58,  0 users,  load average: 0.15, 0.04, 0.01

--- npc/vsrc/bubble.v
+++ npc/vsrc/bubble.v
@@ -7,6 +7,11 @@ module ysyx_22041207_Bubble (
     output reg bubble
 );
 always @(posedge clk) begin
-    bubble <= (me_readNum == 4'b0) ? 1'b0 : (((me_rd == ex_r1 || me_rd == ex_r2) && me_rd != 0) ? 1'b1 : 1'b0);
+    if (me_readNum == 4'b0) begin
+        bubble <= 1'b0;
+    end else begin
+        bubble <= ((me_rd == ex_r1 || me_rd == ex_r2) && me_rd != 0) ? 1'b1 : 1'b0;
+    end
+    
 end
 endmodule
