2d7aa9693101c051ebc550451b546cd19cbc4bba
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 18:56:26 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:56:26 up 10:31,  0 users,  load average: 0.08, 0.02, 0.11

--- npc/vsrc/id_ex.v
+++ npc/vsrc/id_ex.v
@@ -94,7 +94,7 @@ always @(negedge clk) begin
         mcause_o <= 0;
     end
     else if (bubble) begin
-        $display("bubble");
+        $display("rxbubble");
         aluOperate_o <= aluOperate_o;
         sel_a_o <= sel_a_o;
         sel_b_o <= sel_b_o;
