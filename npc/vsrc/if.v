f12ff60af7c182929244711141f42e3eac56c205
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 19:51:10 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:51:10 up 11:26,  0 users,  load average: 0.00, 0.02, 0.00

--- npc/vsrc/if.v
+++ npc/vsrc/if.v
@@ -11,23 +11,25 @@ module ysyx_22041207_IF (
     input [63:0] me_pc,
     input [63:0] me_imm,
     input [63:0] me_r1data,
-    output [31:0] inst_o,
-    output [63:0] pc_o
+    output reg [31:0] inst_o,
+    output reg [63:0] pc_o
 );
 wire [63:0] rawData;
 wire [31:0] inst;
 reg [63:0] pc;
 initial begin
-    pc = 64'h80000000 - 64'h4;
+    pc = 64'h80000000;
 end
 ysyx_22041207_read_mem readInst(pc, 1'b1, rawData);
 assign inst = rawData [31:0];  // 这里可能有BUG
-assign pc_o = pc;
-assign inst_o = inst;
 wire [63:0] addRes;
 assign addRes = me_r1data + me_imm;
 always @(posedge clk) begin
-        if (me_jal || (me_branch && me_aluRes == 0)) begin
+    pc_o <= pc;
+    inst_o <= inst;
+end
+always @(negedge clk) begin
+    if (me_jal || (me_branch && me_aluRes == 0)) begin
             $display("catch jal.. %x %x", me_pc, me_pc + me_imm);
             pc <= me_pc + me_imm;
         end
