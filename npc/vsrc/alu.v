d7a13393228102824359730766c5c39b1b2777a6
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 14:15:29 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:15:29 up 1 day,  5:50,  0 users,  load average: 0.10, 0.03, 0.01

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -22,7 +22,7 @@ always @(posedge clk) begin
     case(operate)
         `ALU_ADD: begin
             res = a + b;
-            $display("add %x %x %x", a, b, a + b);
+            //$display("add %x %x %x", a, b, a + b);
         end
         `ALU_SUB: res = a - b;
         `ALU_RETURN_A: res = a;
