74440f51f94c85027969b534ecda0d395eedee1c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-23 21:26:32 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:26:32 up 37 min,  0 users,  load average: 0.18, 0.14, 0.23

--- npc/vsrc/if.v
+++ npc/vsrc/if.v
@@ -62,7 +62,7 @@ assign addRes = me_r1data + me_imm;
 always @(posedge clk) begin
         if (me_jal || (me_branch && me_aluRes == 0)) begin
             //$display("catch jal.. %x", me_pc + me_imm);
-            $display("catch jal...");
+            //$display("catch jal...");
             pc <= me_pc + me_imm;
         end
         else if (me_jalr) begin // jalr要求最后一位置0
