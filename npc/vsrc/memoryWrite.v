8df64f96de6eda2354f35737300b6e8b04666a27
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-24 19:42:16 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:42:16 up 22:53,  0 users,  load average: 0.31, 0.07, 0.02

--- npc/vsrc/memoryWrite.v
+++ npc/vsrc/memoryWrite.v
@@ -58,6 +58,7 @@ always @(posedge clk) begin
     me_wait_for_axi <= 1;
   end
   if (w_valid_i && w_ready_o) begin
+    $display("axi recieve write");
     w_valid_i <= 0; // axi模块已经收到写入请求
   end
   if (w_valid_o && ~w_ready_i) begin  // 已经完成写入
