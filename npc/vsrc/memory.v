955d2a6c3b6953b487306d599947099eaef930d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 10:41:51 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:41:51 up  6:07,  0 users,  load average: 0.08, 0.06, 0.11

--- npc/vsrc/memory.v
+++ npc/vsrc/memory.v
@@ -43,7 +43,7 @@ always @(posedge clk) begin
     busy <= 1;
     w_valid_i <= 1;
     w_addr_i <= addr;
-    $display("write %x %x", w_addr_i, rs2);
+    
     case (addr[2:0])
     default: w_mask_i <= 0;
     3'h0: w_mask_i <= wmask;
@@ -65,6 +65,7 @@ always @(posedge clk) begin
     w_ready_i <= 1;
   end
   if (w_valid_o && w_ready_i) begin  // 已经完成写入
+    $display("write %x %x", w_addr_i, w_data_i);
     w_ready_i <= 0;
     me_wait_for_axi <= 0;
     busy <= 0;
