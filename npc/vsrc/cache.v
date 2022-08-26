f92da65e2edf64365f54e065569340a8ec3008ea
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 20:59:53 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:53 up  5:44,  0 users,  load average: 0.20, 0.09, 0.03

--- npc/vsrc/cache.v
+++ npc/vsrc/cache.v
@@ -43,6 +43,7 @@ always @(posedge clk) begin
     end
     else begin
         if (updateData) begin
+        $display("updateData");
         // 先去填写0
             if (lastWrite[windex]) begin
                 // 那就写0
