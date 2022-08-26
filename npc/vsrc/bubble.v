99f3663667dbdcbdf56b99877c58822e6310ec17
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 10:20:26 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:26 up  5:45,  0 users,  load average: 0.34, 0.22, 0.39

--- npc/vsrc/bubble.v
+++ npc/vsrc/bubble.v
@@ -13,6 +13,6 @@ always @(*) begin
     end else begin
         bubble = (waitForAXI | ((me_rd == ex_r1 || me_rd == ex_r2) && me_rd != 0)) ? 1'b1 : 1'b0;
     end
-    $display("buuble %x", bubble);
+    $display("buuble %x", waitForAXI);
 end
 endmodule
