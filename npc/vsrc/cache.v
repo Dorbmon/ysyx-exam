4c9342fe518001075ed5465deb218a7cb8ec1c60
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-26 21:16:20 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:16:20 up  6:00,  0 users,  load average: 0.00, 0.04, 0.02

--- npc/vsrc/cache.v
+++ npc/vsrc/cache.v
@@ -36,9 +36,9 @@ always @(posedge clk) begin
     if (rst) begin
         integer i;
         for (i = 0;i < 4;i ++) begin
-            valid0 [i] <= 0;
-            valid1 [i] <= 0;
-            lastWrite [i] <= 0;
+            valid0 [i] = 0;
+            valid1 [i] = 0;
+            lastWrite [i] = 0;
         end
     end
     else begin
@@ -46,45 +46,45 @@ always @(posedge clk) begin
         // 先去填写0
             if (lastWrite[windex]) begin
                 // 那就写0
-                way0 [windex] <= actualData;
-                tag0 [windex] <= wtag;
-                valid0 [windex] <= 1;
+                way0 [windex] = actualData;
+                tag0 [windex] = wtag;
+                valid0 [windex] = 1;
             end else begin
-                way1 [windex] <= actualData;
-                tag1 [windex] <= wtag;
-                valid1 [windex] <= 1;
+                way1 [windex] = actualData;
+                tag1 [windex] = wtag;
+                valid1 [windex] = 1;
             end
             lastWrite [windex] = ~lastWrite [windex];
         end
         if (wUpdateData) begin
             if (lastWrite[wwindex]) begin
                 // 那就写0
-                // [wwindex] <= wActualData;
-                tag0 [wwindex] <= wwtag;
-                valid0 [wwindex] <= 1;
+                // [wwindex] = wActualData;
+                tag0 [wwindex] = wwtag;
+                valid0 [wwindex] = 1;
                 //integer i;
-                way0[wwindex][7: 0] <= wMask[0] ? wActualData [7: 0] : way0 [wwindex][7: 0];
-                way0[wwindex][15: 0] <= wMask[1] ? wActualData [15: 0] : way0 [wwindex][15: 0];
-                way0[wwindex][23: 0] <= wMask[2] ? wActualData [23: 0] : way0 [wwindex][23: 0];
-                way0[wwindex][31: 0] <= wMask[3] ? wActualData [31: 0] : way0 [wwindex][31: 0];
-                way0[wwindex][39: 0] <= wMask[4] ? wActualData [39: 0] : way0 [wwindex][39: 0];
-                way0[wwindex][47: 0] <= wMask[5] ? wActualData [47: 0] : way0 [wwindex][47: 0];
-                way0[wwindex][55: 0] <= wMask[6] ? wActualData [55: 0] : way0 [wwindex][55: 0];
-                way0[wwindex][63: 0] <= wMask[7] ? wActualData [63: 0] : way0 [wwindex][63: 0];
+                way0[wwindex][7: 0] = wMask[0] ? wActualData [7: 0] : way0 [wwindex][7: 0];
+                way0[wwindex][15: 0] = wMask[1] ? wActualData [15: 0] : way0 [wwindex][15: 0];
+                way0[wwindex][23: 0] = wMask[2] ? wActualData [23: 0] : way0 [wwindex][23: 0];
+                way0[wwindex][31: 0] = wMask[3] ? wActualData [31: 0] : way0 [wwindex][31: 0];
+                way0[wwindex][39: 0] = wMask[4] ? wActualData [39: 0] : way0 [wwindex][39: 0];
+                way0[wwindex][47: 0] = wMask[5] ? wActualData [47: 0] : way0 [wwindex][47: 0];
+                way0[wwindex][55: 0] = wMask[6] ? wActualData [55: 0] : way0 [wwindex][55: 0];
+                way0[wwindex][63: 0] = wMask[7] ? wActualData [63: 0] : way0 [wwindex][63: 0];
             end else begin
-                way1 [wwindex] <= wActualData;
-                tag1 [wwindex] <= wwtag;
-                valid1 [wwindex] <= 1;
-                way1[wwindex][7: 0] <= wMask[0] ? wActualData [7: 0] : way1 [wwindex][7: 0];
-                way1[wwindex][15: 0] <= wMask[1] ? wActualData [15: 0] : way1 [wwindex][15: 0];
-                way1[wwindex][23: 0] <= wMask[2] ? wActualData [23: 0] : way1 [wwindex][23: 0];
-                way1[wwindex][31: 0] <= wMask[3] ? wActualData [31: 0] : way1 [wwindex][31: 0];
-                way1[wwindex][39: 0] <= wMask[4] ? wActualData [39: 0] : way1 [wwindex][39: 0];
-                way1[wwindex][47: 0] <= wMask[5] ? wActualData [47: 0] : way1 [wwindex][47: 0];
-                way1[wwindex][55: 0] <= wMask[6] ? wActualData [55: 0] : way1 [wwindex][55: 0];
-                way1[wwindex][63: 0] <= wMask[7] ? wActualData [63: 0] : way1 [wwindex][63: 0];
+                way1 [wwindex] = wActualData;
+                tag1 [wwindex] = wwtag;
+                valid1 [wwindex] = 1;
+                way1[wwindex][7: 0] = wMask[0] ? wActualData [7: 0] : way1 [wwindex][7: 0];
+                way1[wwindex][15: 0] = wMask[1] ? wActualData [15: 0] : way1 [wwindex][15: 0];
+                way1[wwindex][23: 0] = wMask[2] ? wActualData [23: 0] : way1 [wwindex][23: 0];
+                way1[wwindex][31: 0] = wMask[3] ? wActualData [31: 0] : way1 [wwindex][31: 0];
+                way1[wwindex][39: 0] = wMask[4] ? wActualData [39: 0] : way1 [wwindex][39: 0];
+                way1[wwindex][47: 0] = wMask[5] ? wActualData [47: 0] : way1 [wwindex][47: 0];
+                way1[wwindex][55: 0] = wMask[6] ? wActualData [55: 0] : way1 [wwindex][55: 0];
+                way1[wwindex][63: 0] = wMask[7] ? wActualData [63: 0] : way1 [wwindex][63: 0];
             end
-            lastWrite [wwindex] <= ~lastWrite [wwindex];
+            lastWrite [wwindex] = ~lastWrite [wwindex];
         end
     end
 end
