b8b9cb5793b19dd089de33d4b35bbabadd2db988
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-27 20:08:57 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:08:57 up 11:19,  0 users,  load average: 0.09, 0.04, 0.00

--- npc/vsrc/cache.v
+++ npc/vsrc/cache.v
@@ -24,8 +24,11 @@ wire available0 = (valid0[index] && (tag0[index] == tag));
 wire [63:0] read1 = way1[index];
 wire available1 = (valid1[index] && (tag1[index] == tag));
 
-assign readHit = (available0 | available1);
-assign readData = available0 ? read0 : (available1 ? read1 : 0);
+//assign readHit = (available0 | available1);
+//assign readData = available0 ? read0 : (available1 ? read1 : 0);
+assign readHit = (tag0[index] == tag);
+assign readData = read0;
+
 
 wire [58:0] wtag = updateAddress [63:5];
 wire [1:0]  windex = updateAddress [4:3];
@@ -49,47 +52,55 @@ always @(posedge clk) begin
         end
     end
     else begin
-        if (updateData) begin   //读更新
-            //$display("r");
-            if ((lastWrite[windex] || tag0 [windex] == wtag) && tag1 [windex] != wtag) begin
-                // 那就写0
-                way0 [windex] <= actualData;
-                tag0 [windex] <= wtag;
-                valid0 [windex] <= 1;
-            end else begin
-                way1 [windex] <= actualData;
-                tag1 [windex] <= wtag;
-                valid1 [windex] <= 1;
-            end
-            lastWrite [windex] <= ~lastWrite [windex];
+        if (updateData) begin
+            tag0 [windex] <= wtag;
+            way0 [windex] <= actualData;
+            valid0 [windex] <= 1;
         end
         if (wUpdateData) begin
-           // valid0 [wwindex] <= 0;
-           // valid1 [wwindex] <= 0;
-            if ((lastWrite[wwindex] || tag0 [wwindex] == wwtag) && tag1 [wwindex] != wwtag) begin
-                // 那就写0
-                tag0 [wwindex] <= wwtag;
-                way0[wwindex][7: 0] <= wMask[0] ? wActualData [7: 0] : way0 [wwindex][7: 0];
-                way0[wwindex][15: 8] <= wMask[1] ? wActualData [15: 8] : way0 [wwindex][15: 8];
-                way0[wwindex][23: 16] <= wMask[2] ? wActualData [23: 16] : way0 [wwindex][23: 16];
-                way0[wwindex][31: 24] <= wMask[3] ? wActualData [31: 24] : way0 [wwindex][31: 24];
-                way0[wwindex][39: 32] <= wMask[4] ? wActualData [39: 32] : way0 [wwindex][39: 32];
-                way0[wwindex][47: 40] <= wMask[5] ? wActualData [47: 40] : way0 [wwindex][47: 40];
-                way0[wwindex][55: 48] <= wMask[6] ? wActualData [55: 48] : way0 [wwindex][55: 48];
-                way0[wwindex][63: 56] <= wMask[7] ? wActualData [63: 56] : way0 [wwindex][63: 56];
-            end else begin
-                tag1 [wwindex] <= wwtag;
-                way1[wwindex][7: 0] <= wMask[0] ? wActualData [7: 0] : way1 [wwindex][7: 0];
-                way1[wwindex][15: 8] <= wMask[1] ? wActualData [15: 8] : way1 [wwindex][15: 8];
-                way1[wwindex][23: 16] <= wMask[2] ? wActualData [23: 16] : way1 [wwindex][23: 16];
-                way1[wwindex][31: 24] <= wMask[3] ? wActualData [31: 24] : way1 [wwindex][31: 24];
-                way1[wwindex][39: 32] <= wMask[4] ? wActualData [39: 32] : way1 [wwindex][39: 32];
-                way1[wwindex][47: 40] <= wMask[5] ? wActualData [47: 40] : way1 [wwindex][47: 40];
-                way1[wwindex][55: 48] <= wMask[6] ? wActualData [55: 48] : way1 [wwindex][55: 48];
-                way1[wwindex][63: 56] <= wMask[7] ? wActualData [63: 56] : way1 [wwindex][63: 56];
-            end
-            lastWrite [wwindex] <= ~lastWrite [wwindex];
+            valid0 [wwindex] <= 0;
         end
+        // if (updateData) begin   //读更新
+        //     //$display("r");
+        //     if ((lastWrite[windex] || tag0 [windex] == wtag) && tag1 [windex] != wtag) begin
+        //         // 那就写0
+        //         way0 [windex] <= actualData;
+        //         tag0 [windex] <= wtag;
+        //         valid0 [windex] <= 1;
+        //     end else begin
+        //         way1 [windex] <= actualData;
+        //         tag1 [windex] <= wtag;
+        //         valid1 [windex] <= 1;
+        //     end
+        //     lastWrite [windex] <= ~lastWrite [windex];
+        // end
+        // if (wUpdateData) begin
+        //    // valid0 [wwindex] <= 0;
+        //    // valid1 [wwindex] <= 0;
+        //     if ((lastWrite[wwindex] || tag0 [wwindex] == wwtag) && tag1 [wwindex] != wwtag) begin
+        //         // 那就写0
+        //         tag0 [wwindex] <= wwtag;
+        //         way0[wwindex][7: 0] <= wMask[0] ? wActualData [7: 0] : way0 [wwindex][7: 0];
+        //         way0[wwindex][15: 8] <= wMask[1] ? wActualData [15: 8] : way0 [wwindex][15: 8];
+        //         way0[wwindex][23: 16] <= wMask[2] ? wActualData [23: 16] : way0 [wwindex][23: 16];
+        //         way0[wwindex][31: 24] <= wMask[3] ? wActualData [31: 24] : way0 [wwindex][31: 24];
+        //         way0[wwindex][39: 32] <= wMask[4] ? wActualData [39: 32] : way0 [wwindex][39: 32];
+        //         way0[wwindex][47: 40] <= wMask[5] ? wActualData [47: 40] : way0 [wwindex][47: 40];
+        //         way0[wwindex][55: 48] <= wMask[6] ? wActualData [55: 48] : way0 [wwindex][55: 48];
+        //         way0[wwindex][63: 56] <= wMask[7] ? wActualData [63: 56] : way0 [wwindex][63: 56];
+        //     end else begin
+        //         tag1 [wwindex] <= wwtag;
+        //         way1[wwindex][7: 0] <= wMask[0] ? wActualData [7: 0] : way1 [wwindex][7: 0];
+        //         way1[wwindex][15: 8] <= wMask[1] ? wActualData [15: 8] : way1 [wwindex][15: 8];
+        //         way1[wwindex][23: 16] <= wMask[2] ? wActualData [23: 16] : way1 [wwindex][23: 16];
+        //         way1[wwindex][31: 24] <= wMask[3] ? wActualData [31: 24] : way1 [wwindex][31: 24];
+        //         way1[wwindex][39: 32] <= wMask[4] ? wActualData [39: 32] : way1 [wwindex][39: 32];
+        //         way1[wwindex][47: 40] <= wMask[5] ? wActualData [47: 40] : way1 [wwindex][47: 40];
+        //         way1[wwindex][55: 48] <= wMask[6] ? wActualData [55: 48] : way1 [wwindex][55: 48];
+        //         way1[wwindex][63: 56] <= wMask[7] ? wActualData [63: 56] : way1 [wwindex][63: 56];
+        //     end
+        //     lastWrite [wwindex] <= ~lastWrite [wwindex];
+        // end
     end
 end
 endmodule
