0fa5343c098d033becafa48651882ad138c6768f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-27 16:44:45 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:44:45 up  7:55,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/axi.v
+++ npc/vsrc/axi.v
@@ -224,9 +224,9 @@ module axi_rw # (
             // 告知缓存更新
             cache_update_en <= 1;
             if (cache_hit) begin
-                $display("read %x %x %x",r_addr_i, cache_data, axi_r_data_i);
+                $display("read %x %x %x",{r_addr_i[63:3], 3'b0}, cache_data, axi_r_data_i);
             end else begin
-                $display("update %x %x", r_addr_i, axi_r_data_i);
+                $display("update %x %x", {r_addr_i[63:3], 3'b0}, axi_r_data_i);
             end
             //$display("read.. %x", axi_r_data_i);
             r_state_read <= 0;
