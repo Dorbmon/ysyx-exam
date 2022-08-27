c3088a501715dc6ec48c2d949021b121c45b6f4d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-27 08:50:28 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:50:28 up 1 min,  0 users,  load average: 0.09, 0.05, 0.01

--- npc/vsrc/cache.v
+++ npc/vsrc/cache.v
@@ -34,10 +34,10 @@ wire [1:0]  wwindex = wUpdateAddress [4:3];
 initial begin
     integer i;
     for (i = 0;i < 4;i ++) begin
-            valid0 [i] = 0;
-            valid1 [i] = 0;
-            lastWrite [i] = 0;
-        end
+        valid0 [i] = 0;
+        valid1 [i] = 0;
+        lastWrite [i] = 0;
+    end
 end
 always @(posedge clk) begin
     if (rst) begin
@@ -49,25 +49,25 @@ always @(posedge clk) begin
         end
     end
     else begin
-        // if (updateData) begin   //读更新
-        //     if (lastWrite[windex] || tag0 [windex] == wtag) begin
-        //         // 那就写0
-        //         way0 [windex] = actualData;
-        //         tag0 [windex] = wtag;
-        //         valid0 [windex] = 1;
-        //     end else begin
-        //         way1 [windex] = actualData;
-        //         tag1 [windex] = wtag;
-        //         valid1 [windex] = 1;
-        //     end
-        //     lastWrite [windex] = ~lastWrite [windex];
-        // end
+        if (updateData) begin   //读更新
+            if (lastWrite[windex] || tag0 [windex] == wtag) begin
+                // 那就写0
+                way0 [windex] = actualData;
+                tag0 [windex] = wtag;
+                valid0 [windex] = 1;
+            end else begin
+                way1 [windex] = actualData;
+                tag1 [windex] = wtag;
+                valid1 [windex] = 1;
+            end
+            lastWrite [windex] = ~lastWrite [windex];
+        end
         if (wUpdateData) begin
             if (lastWrite[wwindex] || tag0 [wwindex] == wwtag) begin
                 // 那就写0
                 // [wwindex] = wActualData;
                 tag0 [wwindex] = wwtag;
-                valid0 [wwindex] = 1;
+                //valid0 [wwindex] = 1;
                 //integer i;
                 way0[wwindex][7: 0] = wMask[0] ? wActualData [7: 0] : way0 [wwindex][7: 0];
                 way0[wwindex][15: 0] = wMask[1] ? wActualData [15: 0] : way0 [wwindex][15: 0];
@@ -79,7 +79,7 @@ always @(posedge clk) begin
                 way0[wwindex][63: 0] = wMask[7] ? wActualData [63: 0] : way0 [wwindex][63: 0];
             end else begin
                 tag1 [wwindex] = wwtag;
-                valid1 [wwindex] = 1;
+                //valid1 [wwindex] = 1;
                 way1[wwindex][7: 0] = wMask[0] ? wActualData [7: 0] : way1 [wwindex][7: 0];
                 way1[wwindex][15: 0] = wMask[1] ? wActualData [15: 0] : way1 [wwindex][15: 0];
                 way1[wwindex][23: 0] = wMask[2] ? wActualData [23: 0] : way1 [wwindex][23: 0];
