05db393b96942e2096ab2d8ec705ecd0de841727
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-27 09:03:56 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:03:56 up 14 min,  0 users,  load average: 0.05, 0.06, 0.01

--- npc/vsrc/cache.v
+++ npc/vsrc/cache.v
@@ -45,33 +45,30 @@ always @(posedge clk) begin
     if (rst) begin
         integer i;
         for (i = 0;i < 4;i ++) begin
-            valid0 [i] = 0;
-            valid1 [i] = 0;
-            lastWrite [i] = 0;
+            valid0 [i] <= 0;
+            valid1 [i] <= 0;
+            lastWrite [i] <= 0;
         end
     end
     else begin
-        if (updateData && wUpdateData)
-            $display("first");
-
         if (updateData) begin   //读更新
             //$display("r");
             if (lastWrite[windex] || tag0 [windex] == wtag) begin
                 // 那就写0
-                way0 [windex] = actualData;
-                tag0 [windex] = wtag;
-                valid0 [windex] = 1;
+                way0 [windex] <= actualData;
+                tag0 [windex] <= wtag;
+                valid0 [windex] <= 1;
             end else begin
-                way1 [windex] = actualData;
-                tag1 [windex] = wtag;
-                valid1 [windex] = 1;
+                way1 [windex] <= actualData;
+                tag1 [windex] <= wtag;
+                valid1 [windex] <= 1;
             end
-            lastWrite [windex] = ~lastWrite [windex];
+            lastWrite [windex] <= ~lastWrite [windex];
         end
         if (wUpdateData) begin
             //$display("ee");
-            valid0 [wwindex] = 0;
-            valid1 [wwindex] = 0;
+            valid0 [wwindex] <= 0;
+            valid1 [wwindex] <= 0;
         //     if (lastWrite[wwindex] || tag0 [wwindex] == wwtag) begin
         //         // 那就写0
         //         // [wwindex] = wActualData;
