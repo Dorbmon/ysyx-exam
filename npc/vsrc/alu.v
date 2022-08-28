7d86e7c3cef5dfca1ba8e17fe86d98ad2aae22cd
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-28 21:22:43 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:22:43 up 12:26,  0 users,  load average: 0.08, 0.02, 0.01

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -68,7 +68,7 @@ always @(posedge clk) begin
             if (ccc == 32'd32 && alu_wait) begin
                 alu_wait <= 0;
                 //res <= a * b;
-                res <= mul_res;
+                res <= a * b;
                 $display ("%x finish %d %d %d %d", pc, a, b, mul_res, a*b);
             end
             // if (alu_wait) begin
