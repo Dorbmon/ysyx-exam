512869e45f09691305a2cf2142f0e1b8ff3f76f8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-28 20:21:39 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:21:39 up 11:25,  0 users,  load average: 0.01, 0.01, 0.00

--- npc/vsrc/mul.v
+++ npc/vsrc/mul.v
@@ -43,7 +43,7 @@ always @(posedge clk) begin
         if (~flush && ~mul_ready && count == 8'h3F && ~out_valid) begin
             //mul_res <= mul_res + ((l_multiplier[0]) ? l_multiplicand : 0);
             //mul_res <= multiplicand * multiplier;
-            //$display("mul %x %x %x", multiplicand, multiplier, mul_res + ((l_multiplier[0]) ? l_multiplicand : 0));
+            $display("mul %x %x %x", multiplicand, multiplier, mul_res);
             out_valid <= 1;
             mul_ready <= 1;
         end
