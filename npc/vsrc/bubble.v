b57838f1b8d4b1b57d8f18d3fbe4b2b57d5d991b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-28 14:46:16 +0800

    > sim RTL ysyx_22040000 张三 Linux Dorbmon-PC 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:16 up  5:49,  0 users,  load average: 0.12, 0.05, 0.01

--- npc/vsrc/bubble.v
+++ npc/vsrc/bubble.v
@@ -1,6 +1,6 @@
 module ysyx_22041207_Bubble (
     input clk,
-    input ex_alu_valid,
+    input ex_alu_wait,
     input [4:0] ex_r1,
     input [4:0] ex_r2,
     input [4:0] me_rd,
@@ -12,7 +12,10 @@ always @(*) begin
     if (me_readNum == 4'b0) begin
         bubble = 1'b0;
     end else begin
-        bubble = (ex_alu_valid | waitForAXI | ((me_rd == ex_r1 || me_rd == ex_r2) && me_rd != 0)) ? 1'b1 : 1'b0;
+        if (ex_alu_wait) begin
+            $display("alu wait..");
+        end
+        bubble = (ex_alu_wait | waitForAXI | ((me_rd == ex_r1 || me_rd == ex_r2) && me_rd != 0)) ? 1'b1 : 1'b0;
     end
 end
 endmodule
